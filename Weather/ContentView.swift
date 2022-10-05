f910818ed92afe5287906d8f9a6dbc1734609b39
sandy <sandy@studio-sj.co>
2022-10-05 10:12:45 +0900

    Initial Commit

---
+++ Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Studio-SJ on 2022/10/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
